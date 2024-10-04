db1f865b6ed2b2f118b2a7ceb679302243238661
YashasMangalore <129035779+YashasMangalore@users.noreply.github.com>
2024-10-04 21:26:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,21 +5,20 @@ class Person {
     }
 
     greet() {
-        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+        return `Hello, my name is ${this.name} and I am ${this.age} years old.`; // Updated format to match expected output
     }
 }
 
 class Employee extends Person {
     constructor(name, age, jobTitle) {
-        super(name, age);  // Call the constructor of the parent class (Person)
+        super(name, age);
         this.jobTitle = jobTitle;
     }
 
     jobGreet() {
-        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+        return `Hello, my name is ${this.name} and I am ${this.age} years old, and my job title is ${this.jobTitle}.`; // Make sure the format matches expected output
     }
 }
 
-// Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
