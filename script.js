031a72acc39e5a9ee3b0b31cc1230f145e46c09f
YashasMangalore <129035779+YashasMangalore@users.noreply.github.com>
2024-10-05 11:07:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ class Employee extends Person {
     }
 
     jobGreet() {
-        if (this.name !== "Alice") {
+        if (this.name === "Alice") {
             console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
         } else {
             this.greet(); // Call the greet method from the parent class if no job title is provided
