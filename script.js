fdf86fbc332e52e277e04e7c308766a4ebdf791e
YashasMangalore <129035779+YashasMangalore@users.noreply.github.com>
2024-10-05 11:23:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,66 @@
-class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
 
-    greet() {
-        console.log(`Hello, my name is Alice and I am 25 years old.`);
-    }
+//     greet() {
+//         console.log(`Hello, my name is Alice and I am 25 years old.`);
+//     }
+// }
+
+// class Employee extends Person {
+//     constructor(name, age, jobTitle) {
+//         super(name, age);  // Call the constructor of the parent class (Person)
+//         this.jobTitle = jobTitle;
+//     }
+
+//     jobGreet() {
+//         if (this.jobTitle) {
+//             console.log(`Hello, my name is Bob, I am 30 years old, and my job title is Manager.`);
+//         } else {
+//             this.greet(); // Call the greet method from the parent class if no job title is provided
+//         }
+//     }
+// }
+
+// // Do not change code below this line
+// window.Person = Person;
+// window.Employee = Employee;
+
+
+
+
+function Person(name, age) {
+this.name = name;
+this.age = age;
 }
 
-class Employee extends Person {
-    constructor(name, age, jobTitle) {
-        super(name, age);  // Call the constructor of the parent class (Person)
-        this.jobTitle = jobTitle;
-    }
+Person.prototype.greet = function () {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+   // console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+};
 
-    jobGreet() {
-        if (this.jobTitle) {
-            console.log(`Hello, my name is Bob, I am 30 years old, and my job title is Manager.`);
-        } else {
-            this.greet(); // Call the greet method from the parent class if no job title is provided
-        }
-    }
+function Employee(name, age, jobTitle) {
+Person.call(this, name, age);
+this.jobTitle = jobTitle;
 }
+
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function () {
+console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
 
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
 
-
-
-// // Test case 1: Person instance
 // const person = new Person("Alice", 25);
-// person.greet();
-
-// // Test case 2: Employee instance
 // const employee = new Employee("Bob", 30, "Manager");
-// employee.jobGreet();
+
+// person.greet(); 
+// // employee.greet(); 
+// employee.jobGreet(); 
+
